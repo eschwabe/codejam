9f3b3e12b6272dfd98693c7646be447466979900
Eric Schwabe <eschwabe@gmail.com>
2014-09-14 10:08:48 -0700

    Fix additional qual c bugs. Certain solvable puzzles are still not handled.

--- 2014/qual-c.cpp
+++ 2014/qual-c.cpp
@@ -1,22 +1,17 @@
 // Problem C. Minesweeper Master
 // https://code.google.com/codejam/contest/2974486/dashboard#s=p2
 
-// TODO: 
-// dynamically allocate array (two arrays?)
-// keep track of adajacent ce
-
 #include <cmath>
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <vector>
 
-const int kMaxDimension = 50;
+typedef std::vector< std::vector<int> > Puzzle;
 
 enum CellType {
-    FREE = 0,
-    CLICK,
-    MINE,
+    MINE = -1
 };
 
 // read a line from stdin
@@ -26,14 +21,14 @@ std::stringstream ReadLine() {
     return std::stringstream(tmp);
 }
 
-// count the number of adjacent mines and free cells
+// count the number of adjacent free and mine cells
 std::tuple<int, int> CountAdjacent(
-        int rows,
-        int cols,
         int row,
         int col,
-        int grid[kMaxDimension][kMaxDimension]) {
+        Puzzle& grid) {
 
+    auto rows = static_cast<int>(grid.size());
+    auto cols = static_cast<int>(grid[0].size());
     int mine_cells = 0;
     int free_cells = 0;
 
@@ -43,36 +38,61 @@ std::tuple<int, int> CountAdjacent(
                 continue;
             } else if(grid[r][c] == MINE) {
                 mine_cells++;
-            } else if(grid[r][c] == FREE) {
+            } else if(grid[r][c] != MINE) {
                 free_cells++;
             }
         }
     }
 
-    return std::tuple<int, int>(free_cells, mine_cells);
+    return std::make_tuple(free_cells, mine_cells);
+}
+
+// place new mine on grid and update stats
+void PlaceMine(
+        int row,
+        int col,
+        Puzzle& grid) {
+
+    auto rows = static_cast<int>(grid.size());
+    auto cols = static_cast<int>(grid[0].size());
+
+    for(int r = std::max(row-1,0); r <= std::min(row+1, rows-1); ++r) {
+        for(int c = std::max(col-1,0); c <= std::min(col+1, cols-1); ++c) {
+            if(r == row && c == col) {
+                grid[row][col] = MINE;
+            } else if(grid[r][c] != MINE) {
+                grid[r][c]++;
+            }
+        }
+    }
 }
 
 // solve minesweeper puzzle
 // return true if solvable, false if not solvable
-bool SolvePuzzle(int rows, int cols, int mines, int grid[kMaxDimension][kMaxDimension]) {
+void GeneratePuzzle(int mines, Puzzle& grid) {
+
+    auto rows = static_cast<int>(grid.size());
+    auto cols = static_cast<int>(grid[0].size());
 
     for(int m = 0; m < mines; ++m) {
 
         // scan for new mine position with least impact
         int target_row = -1;
         int target_col = -1;
-        std::tuple<int, int> target_stats(8,8);
+        auto target_stats = std::make_tuple(9,9);
 
         for(int row = 0; row < rows; ++row) {
             for(int col = 0; col < cols; ++col) {
 
                 // count adjacent cells
-                if(grid[row][col] == FREE) {
-                    std::tuple<int, int> stats = CountAdjacent(rows, cols, row, col, grid);
+                if(grid[row][col] != MINE) {
+                    auto stats = CountAdjacent(row, col, grid);
 
-                    // if fewer adjacent free cells or more adjacent mine cells, set as target to mine
+                    // if fewer adjacent free cells or equal free cells and more adjacent mine cells
+                    // set as target to mine
                     if( std::get<0>(stats) < std::get<0>(target_stats) ||
-                        (std::get<0>(stats) == std::get<0>(target_stats) && std::get<1>(stats) > std::get<1>(target_stats)) ) {
+                        (std::get<0>(stats) == std::get<0>(target_stats) && 
+                         std::get<1>(stats) > std::get<1>(target_stats)) ) {
                         target_stats = stats;
                         target_row = row;
                         target_col = col;
@@ -83,44 +103,62 @@ bool SolvePuzzle(int rows, int cols, int mines, int grid[kMaxDimension][kMaxDime
         }
 
         // set new free cell
-        grid[target_row][target_col] = MINE;
-
+        PlaceMine(target_row, target_col, grid);
     }
+}
+
+// check if puzzle solvable in a single click
+bool TestPuzzle(int mines, Puzzle& grid) {
 
     bool solvable = true;
+    auto rows = static_cast<int>(grid.size());
+    auto cols = static_cast<int>(grid[0].size());
 
+    // check if only a single mine
     if(rows*cols != mines+1) {
+
+        // check each cell
         for(int row = 0; row < rows; ++row) {
             for(int col = 0; col < cols; ++col) {
-                solvable = false;
-                for(int r = std::max(row-1,0); r <= std::min(row+1, rows-1); ++r) {
-                    for(int c = std::max(col-1,0); c <= std::min(col+1, cols-1); ++c) {
-                        if(grid[r][c] == FREE) {
-                            std::tuple<int, int> stats = CountAdjacent(rows, cols, r, c, grid);
-                            if(std::get<1>(stats) == 0) {
+
+                if( grid[row][col] != MINE ) {
+                    solvable = false;
+                    // test if cell has an adjacent cell with no surrounding mines
+                    for(int r = std::max(row-1,0); r <= std::min(row+1, rows-1); ++r) {
+                        for(int c = std::max(col-1,0); c <= std::min(col+1, cols-1); ++c) {
+                            if(grid[r][c] == 0) {
                                 solvable = true;
                             }
                         }
                     }
+
+                    // puzzle not solvable if any cell not next to 0 free cell
+                    if(!solvable) {
+                        return solvable;
+                    }
                 }
             }
         }
-    }
 
+    }
 
     return solvable;
 }
 
 // print puzzle contents
-void PrintPuzzle(int rows, int cols, int grid[kMaxDimension][kMaxDimension]) {
-    for(int row = 0; row < rows; ++row) {
-        for(int col = 0; col < cols; ++col) {
-            if(grid[row][col] == FREE) {
-                std::cout << ".";
-            } else if(grid[row][col] == MINE) {
+void PrintPuzzle(Puzzle& grid) {
+    auto clicked = false;
+    for(auto row : grid) {
+        for(auto cell : row) {
+            if(cell == MINE) {
                 std::cout << "*";
-            } else if(grid[row][col] == CLICK) {
+            } else if(cell == 0 && !clicked) {
                 std::cout << "c";
+                clicked = true;
+            }
+            else {
+                //std::cout << ".";
+                std::cout << cell;
             }
         }
         std::cout << std::endl;
@@ -135,28 +173,24 @@ int main()
 
     for(int test = 1; test <= num_tests; ++test) {
         int rows, cols, mines = 0;
-        int grid[kMaxDimension][kMaxDimension];
 
         line = ReadLine();
         line >> rows >> cols >> mines;
 
         // initialize grid with free cells and clicked cell
-        for(int row = 0; row < rows; ++row) {
-            for(int col = 0; col < cols; ++col) {
-                grid[row][col] = FREE;
-            }
-        }
-        grid[rows-1][cols-1] = CLICK;
+        auto grid = Puzzle(rows, std::vector<int>(cols, 0));
 
         // solve puzzle
-        bool result = SolvePuzzle(rows, cols, mines, grid);
+        GeneratePuzzle(mines, grid);
+        bool result = TestPuzzle(mines, grid);
         std::cout << "Case #" << test << ":" << std::endl;
         if(result) {
             std::cout << "Solved" << std::endl;
+            PrintPuzzle(grid);
         } else {
             std::cout << "Impossible" << std::endl;
+            PrintPuzzle(grid);
         }
-        PrintPuzzle(rows, cols, grid);
 
     }
 
